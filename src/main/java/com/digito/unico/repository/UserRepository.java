c72e6cacd537a843b59504c6d10955da1ba6352e
Yago Maia <yagogmaia@gmail.com>
2020-12-17 23:07:41 -0300

    Criação do UserRepository

---
+++ src/main/java/com/digito/unico/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.digito.unico.repository;
+
+import com.digito.unico.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
