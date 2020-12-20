235f50575a96dc98b701fd6e7b0ff01a17aa3b28
Yago Maia <yagogmaia@gmail.com>
2020-12-19 23:31:13 -0300

    Criação dos ambientes de dev e prod

--- src/main/java/com/digito/unico/DigitoUnicoApplication.java
+++ src/main/java/com/digito/unico/DigitoUnicoApplication.java
@@ -2,12 +2,19 @@ package com.digito.unico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DigitoUnicoApplication {
+public class DigitoUnicoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitoUnicoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DigitoUnicoApplication.class);
 	}
 
 }
